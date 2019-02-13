b15328a381cb61b0e3e2802fe850ef509e1783d4
Benjamin <bengyup@gmail.com>
2019-02-13 21:11:07 +1100

    made fnExt tuple fail properly and renamed

--- touch.hs
+++ touch.hs
@@ -1,8 +1,8 @@
 import Text.ParserCombinators.Parsec
 import Control.Monad (liftM2)
 
-pair :: CharParser () (String, Maybe String)
-pair = liftM2 (,) (many $ noneOf ".") (optionMaybe (char '.' >> many anyChar))
+fnExt :: CharParser () (String, Maybe String)
+fnExt = liftM2 (,) (many1 $ noneOf ".") (optionMaybe $ char '.' >> many anyChar)
 
 fn :: String -> Either ParseError (String, Maybe String)
-fn input = parse pair "" input
+fn input = parse fnExt "" input
